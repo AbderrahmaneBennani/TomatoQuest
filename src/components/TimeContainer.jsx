565ee89c01cd3dfee4942e78de3a0d7d2965140b
Abderrahmane Bennani <93011714+AbderrahmaneBennani@users.noreply.github.com>
2024-07-14 22:25:52 +0200

    fix css

--- src/components/TimeContainer.jsx
+++ src/components/TimeContainer.jsx
@@ -3,10 +3,10 @@ import Timer from "./Timer";
 
 const TimeContainer = () => {
   return (
-    <div className="border-2 rounded-xl w-1/2 h-1/2 m-auto flex items-center justify-center flex-col">
-      <div className="flex justify-center items-center mb-12">
+    <div className="border-2 rounded-xl w-1/2 h-1/2 min-w-max max-w-2xl m-auto flex items-center justify-center flex-col">
+      <div className="flex justify-center items-center mx-2 mb-12">
         <img className="w-11 h-11 mx-2" src="/tomato.svg" alt="tomatoIcon" />
-        <h1 className="text-6xl">TomatoQuest</h1>
+        <h1 className="text-6xl mx-2">TomatoQuest</h1>
       </div>
       <Timer />
     </div>
